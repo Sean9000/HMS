e2672121a123809efc608d8aba81f6a5cacf02f1
eucn <eubertnovencido20@gmail.com>
2023-03-02 00:16:22 +0800

    add reservation model

---
+++ app/Models/Reservation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    use HasFactory;
+    public function guest()
+    {
+        return $this->belongsTo(User::class, 'guest_id');
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class, 'room_id');
+    }
+    
+}
