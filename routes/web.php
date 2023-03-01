8f843d5cbe66426e0c564cce556c7a570dc0075d
eucn <eubertnovencido20@gmail.com>
2023-03-02 00:20:04 +0800

    new routes for guest pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Invoice_Controller;
+use App\Http\Controllers\G_Info_Controller;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\GuestController;
+use App\Http\Controllers\Guest_Controller;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -51,37 +53,36 @@ Route::get('/dashboard', function () {
 
 Route::get('/redirects', [HomeController::class, 'index']);
 
-//----------------------------Guest Routes-----------------------------
-// Route::post('/guest_users', 'GuestController@save')->name('save_date');
+    //----------------------------Guest Routes-----------------------------//
 
-Route::post('/guest_users/room_types', [GuestController::class, 'passCheckInDate'])->name('store_date');
+    Route::post('/guest_users/room_types', [Guest_Controller::class, 'passCheckInDate'])->name('store_date');
+   
+    Route::get('/guest_users/room_types', [Guest_Controller::class, 'viewRoomTypes'])->name('room_types');
+    
+    Route::post('/guest_users/room_info1', [Guest_Controller::class, 'type_to_info'])->name('show_room_info1');
+    Route::post('/guest_users/room_info2', [Guest_Controller::class, 'type_to_info'])->name('show_room_info2');
 
-Route::get('/guest_users/room_types', [GuestController::class, 'viewRoomTypes'])->name('room_types');
+    Route::get('/guest_users/room_info', [Guest_Controller::class, 'viewRoomInfo1'])->name('guest_reg');
 
-Route::post('/guest_users/room_info', [GuestController::class, 'type_to_info'])->name('show_room_info');
+    Route::get('/guest_users/room_info', [Guest_Controller::class, 'viewRoomInfo2'])->name('guest_reg');
 
-Route::get('/guest_users/room_info', [GuestController::class, 'viewRoomInfo'])->name('guest_reg');
+    Route::post('/guest_users/guest_registration', [Guest_Controller::class, 'save_Registration'])->name('save_registration');
 
-Route::post('/guest_users/guest_registration', [GuestController::class, 'Show_Regitration'])->name('registration_form');
+    Route::get('/guest_users/guest_registration', [Guest_Controller::class, 'view_guest_info'])->name('registration_form');
 
-Route::get('/guest_users/guest_registration', [GuestController::class, 'Show_Regitration'])->name('registration_form');
+    Route::post('/guest_users/guest_information', [G_Info_Controller::class, 'save_G_Info'])->name('store_guest_info');
 
-// Route::post('/guest_users', [GuestController::class, 'type_to_info'])->name('show_room_info');
-
-// Route::get('/guest_users', [GuestController::class, 'ViewroomInfo'])->name('room_info');
-
-
-
-
-
-// Route::post('/guest', [GuestController::class, 'passCheckInDate'])->name('store-dates');
+    // Route::post('/guest_users/invoice', [Guest_Information_Controller::class, 'save_G_Info'])->name('store_guest_info');
 
 
+    Route::get('/guest_users/invoice', [Invoice_Controller::class, 'view_invoice'])->name('view_invoice');
+    
+    Route::get('/auth', [Guest_Controller::class, 'viewLogin'])->name('login_User');
 
+     //----------------------------Invoice-----------------------------
 
-// Route::post('/guest', [GuestController::class, 'from_roomTypes'])->name('show_date');
-
-Route::get('/auth', [GuestController::class, 'viewLogin'])->name('login_User');
+     Route::post('/guest_users/invoice', [Invoice_Controller::class, 'save_invoice'])->name('store_invoice');
+Route::get('/auth', [Guest_Controller::class, 'viewLogin'])->name('login_User');
 
 // --------------------------User Controller--------------------------
 
