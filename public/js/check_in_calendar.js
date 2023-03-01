513af118e932dc3251ce7f0b281124755fdef466
eucn <eubertnovencido20@gmail.com>
2023-03-01 21:25:32 +0800

    new layout for guest dashboard

--- public/js/check_in_calendar.js
+++ public/js/check_in_calendar.js
@@ -1,5 +1,8 @@
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar1');
+    var calendarEl = document.getElementById('calendar');
   
     var calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'dayGridMonth',
@@ -8,18 +11,60 @@ document.addEventListener('DOMContentLoaded', function() {
         center: 'title',
         right: 'next'
       },
+      // weekends: false,
       selectable: true,
-      select: function(info){
+      selectAllow: function(info) {
+        var start = moment(info.start);
+        var end = moment(info.end);
   
-        var checkin  = info.start;
-        var selectedDate = checkin.toLocaleDateString();
-        // var selectedDate = checkin.format('DD-MM-YYYY');
-        document.getElementById('check_in').value = selectedDate;
-       
+        // Check if the selected range includes any past dates
+        if (start.isBefore(moment(), 'day')) {
+          return false;
+        }
+    
+        return true;
       },
- 
+      eventDidMount: function(info) {
+        var date = moment(info.event.start).startOf('day');
+        var today = moment().startOf('day');
+        if (date.isBefore(today) || date.day() === 0 || date.day() === 6) {
+          info.el.classList.add('fc-day-disabled');
+        }
+      },
+
+      select: function(info){
+        // var checkin  = info.start;
+        // var selectedDate = checkin.toLocaleDateString();
+        // // var selectedDate = checkin.format('DD-MM-YYYY');
+        // document.getElementById('check_in').value = selectedDate;
+        if (info.startStr !== '') {
+          var startDate = moment(info.startStr).format('YYYY-MM-DD');
+          document.getElementById('check_in_date').value = startDate;
+        }
+        if (info.endStr !== '') {
+          var endDate = moment(info.endStr).subtract(1, 'days').format('YYYY-MM-DD');
+          document.getElementById('check_out_date').value = endDate;  
+        }
+        
+        // if (info.startStr !== '') {
+        //   setCheckinDate(info.startStr);
+        // }
+        // if (info.endStr !== '') {
+        //   setCheckoutDate(info.endStr);
+        // }
+        calendar.unselect();
+      },
     });
   
     calendar.render();
   });
+  // var dateStr;
+  // function setCheckinDate(dateStr) {
+  //   document.getElementById('check_in_date').value = moment(dateStr).format('MM/DD/YYYY');
+  // }
+  
+  // function setCheckoutDate(dateStr) {
+    
+  //   document.getElementById('check_out_date').value = moment(dateStr).subtract(1, 'days').format('MM/DD/YYYY');
+  // }
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,126 +1,140 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        @vite('resources/css/app.css')
-        <title>Calendar</title>
-        {{-- Bootstrap theme:Full Calendar --}}
-        
-        {{-- Calendar Css --}}
-        <link rel="stylesheet" href="css/calendar1.css">
-     
 
-        {{-- javascript calendar --}}
-        <script src= "{{url('js/check_in_calendar.js')}}"></script>
-        <script src= "{{url('js/check_out_calendar.js')}}"></script>
-    
-        {{-- Calendar script --}}
-        <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.4/index.global.min.js'></script>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  <title>Calendar</title>
+  {{-- Bootstrap theme:Full Calendar --}}
 
-        {{-- Font awesome --}}
-        <script src="https://kit.fontawesome.com/d0fec6f98b.js" crossorigin="anonymous"></script>
-        
+  {{-- Calendar Css --}}
+  {{-- <link rel="stylesheet" href="css/calendar1.css"> --}}
 
-        <!-- Logo -->   
-        <link rel="icon" type="image/png" sizes="16x16" href= "../images/logo.png">
-        
-    </head>
+  {{-- javascript calendar --}}
+  <script src="{{url('js/check_in_calendar.js')}}"></script>
+  {{-- <script src="{{url('js/datepicker.js')}}"></script> --}}
+  {{-- <script src="{{url('js/check_out_calendar.js')}}"></script> --}}
 
-    <body>
-        {{-- App.blade.php --}}
-        <x-app-layout>
-            <div class="flex items-center justify-center my-10 mx-10 pb-3 bg-gray-100">
-                <div class="w-4/5 flex flex-col bg-gray-200  rounded-2xl p-10 ">
-{{-------------------------------------------------- FORM DATE PICKER -----------------------------------------------}}
-                    <form method="POST" action="{{ route('store_date') }}" required>
+  {{-- Calendar script --}}
+  <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.4/index.global.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js'></script>
 
-                        @csrf
-                        <div class="py-2">
-                            <span class="mb-2 text-md font-semibold">Check-in</span>
-                         
-                            <input
-                                readonly
-                                type="text"
-                                name="check_in_date"
-                                value="{{ old('check_in_date') }}"
-                                class="btn w-full"
-                                id="check_in"
-                                placeholder="DD-MM-YY"
-                                >
-                                
-                                <x-input-error :messages="$errors->get('check_in_date')" class="mt-2 font-base" />                                  
-                        </div>
-                       
-                        <div class="py-2">
-                            <span class="mb-2 text-md font-semibold">Check-out</span>
-                            <input
-                                readonly
-                                type="text"
-                                name="check_out_date"
-                                value="{{ old('check_out_date') }}"
-                                class="w-full"
-                                id="check_out"
-                                placeholder="DD-MM-YY"
-                                />
-                                <x-input-error :messages="$errors->get('check_out_date')" class="mt-2 font-base" />
-                               
-                            </div>
-                           
-              
-{{--------------------------------------------------FULL CALENDAR 1 -----------------------------------------------}}
-                <div class="flex pt-10 space-x-[90px] justify-center  rounded">
+  {{-- Font awesome --}}
+  <script src="https://kit.fontawesome.com/d0fec6f98b.js" crossorigin="anonymous"></script>
 
-                    <div class="">
-                        <div id="calendar1" ></div>
-                    </div>
-                                    
-{{--------------------------------------------------FULL CALENDAR 2 -----------------------------------------------}}
-                        
-                    <div class="">
-                        <div id="calendar2" class="calendar"></div>
+  <!-- Logo -->
+  <link rel="icon" type="image/png" sizes="16x16" href="../images/logo.png">
 
-                    </div>
-                </div>  
+  <style>
+    .fc-day-disabled {
+      color: grey
+    }
+  </style>
+</head>
 
-{{-------------------------------------------------- SECTION LEGEND -----------------------------------------------}}
-                <div class="pt-4">
-                    <div class=" py-4 ">
-                        <div class="px-10 font-bold font-base ">LEGEND</div>
-
-                        <div class="flex items-center px-10 py-4 ">
-
-                            <div class="bg-gray-300 box-border  border-black h-3 w-2.5 p-2 " style="color: #DDDDDD;"></div>
-                            <div for="box" class="pl-3 font-xs">Available Date</div>
-
-                            <div class="bg-blue-600 box-border border-black h-3 w-2.5 p-2 ml-4"></div>
-                            <div for="box" class="pl-3 font-xs">Check-in Date</div>
-
-                        </div>
-                        <div class="flex items-center px-10 ">
-                            <div class="bg-gray-600 box-border border-black h-3 w-2.5 p-2 "></div>
-                            <div for="box" class="pl-3 font-xs">Invalid Date</div>
-                            <div class="flex items-center px-5 ">
-                            <div class="bg-red-600 box-border border-black h-3 w-2.5 p-2 ml-4 "></div>
-                            <div for="box" class="pl-3 font-xs">Check-out Date</div>
-                        </div>
-                    </div>
+<body>
+  {{-- App.blade.php --}}
+  <x-app-layout>
+    <div class="flex items-center justify-center my-10 mx-10 pb-3 bg-gray-100">
+      <div class="w-4/5 flex flex-col bg-gray-200 rounded-2xl p-5">
+        <form method="POST" action="{{ route('store_date') }}">
+          @csrf
+          <div class="md:flex-row justify-center">
+            <div class="w-full px-10 mx-auto ">
+              <div class="grid grid-cols-1 ">
+                <div class=" py-2">
+                  <label class="block text-gray-900 font-medium mb-2" for="check-in-date">Check-in date:</label>
+                  <input
+                    class="w-full border-gray-900 rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                    id="check_in_date" name="check_in_date" type="date" value="{{ old('check_in_date') }}" required>
                 </div>
-                  
-{{-------------------------------------------------- BUTTON -----------------------------------------------}}
-              
-        
-                    <div class=" flex justify-end m-auto">
-                        <button type="submit" 
-                        class="bg-[#E0C822] hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded mt-3 ">
-                            Continue
-                        </button>
-                    </div>
-                </form>
+              </div>
+
+              <div class="grid grid-cols-1 gap-2 mt-1">
+                <div class="py-2">
+                  <label class="block text-gray-900 font-medium mb-2" for="check-out-date">Check-out date:</label>
+                  <input
+                    class="w-full border-gray-400 rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                    id="check_out_date" name="check_out_date" type="date" value="{{ old('check_out_date') }}" required>
+                </div>
+              </div>
+
+              <div class="grid grid-cols-1 mt-1">
+                <div class=" py-2 flex items-center">
+                  <label class="block text-gray-900 font-medium mr-4" for="number-of-nights">Number of Nights:</label>
+                  <input type="" id="number_of_nights" name="number_of_nights"
+                    class="bg-transparent pointer-events-none rounded py-2 px-3 text-gray-900 leading-tight focus:outline-none focus:border-blue-500"
+                    readonly>
+                </div>
+              </div>
             </div>
-        </div>
-    </x-app-layout>
- </body>
+
+            <div class="w-full md:w-1/2 px-4 mb-4 md:mb-0 mx-auto">
+              <div class="bg-white rounded-lg shadow-lg p-4">
+                <h2 class="text-lg font-medium mb-4 pb-2 border-b border-gray-300 text-center">Date Availability</h2>
+                <div id="calendar"></div>
+              </div>
+              <div class=" py-4 ">
+
+                <div class="px-10 font-bold font-base">LEGEND</div>
+
+                <div class="flex items-center px-10 py-2 ">
+                  <div class="bg-gray-300 box-border  border-black h-3 w-2.5 p-2 " style="color: #DDDDDD;"></div>
+                  <div for="box" class="pl-3 font-xs md:w-1/2">Available Date</div>
+
+                  <div class="bg-blue-600 box-border border-black h-3 w-2.5 p-2 ml-4"></div>
+                  <div for="box" class="pl-3 font-xs md:w-1/2">Check-in Date</div>
+                </div>
+
+                <div class="flex items-center px-10 py-2">
+                  <div class="bg-gray-600 box-border border-black h-3 w-2.5 p-2 "></div>
+                  <div for="box" class="pl-3 font-xs md:w-1/2">Not Available </div>
+
+                  <div class="bg-red-600 box-border border-black h-3 w-2.5 p-2 ml-4"></div>
+                  <div for="box" class="pl-3 font-xs md:w-1/2">Check-out Date</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex items-center justify-end">
+            <div class="w-full md:w-1/2 px-4 ">
+              <div class="flex flex-row-reverse mt-5 mb-10">
+                <button  class="bg-yellow-500 text-white active:bg-yellow-800 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="submit"
+                  required>Continue</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    </div>
+
+    </div>
+
+    {{-- script for date --}}
+    <script>
+      const check_in_date = document.getElementById('check_in_date');
+      const check_out_date = document.getElementById('check_out_date');
+      check_in_date.min = new Date().toISOString().split('T')[0];
+      check_out_date.min = new Date().toISOString().split('T')[0];
+    </script>
+
+    <script>
+      const checkInDate = document.getElementById('check_in_date');
+      const checkOutDate = document.getElementById('check_out_date');
+      const numberOfNights = document.getElementById('number_of_nights');
+      checkOutDate.addEventListener('change', function() {
+        const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
+        const checkIn = new Date(checkInDate.value);
+        const checkOut = new Date(checkOutDate.value);
+        const diffDays = Math.round(Math.abs((checkOut - checkIn) / oneDay));
+        numberOfNights.value = diffDays;
+      });
+    </script>
+  </x-app-layout>
+</body>
 
 </html>
